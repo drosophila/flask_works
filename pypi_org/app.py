1d8860da3ec951240583c2c35799f3840eecabbc
drosophila <drosophilaris@gmail.com>
2019-12-13 23:21:56 -0600

    added css js bootstrap

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -2,14 +2,21 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route("/")
-def index(): #the def is the controller referred to as the view method
-    test_packages = [
-        'package1',
-        'package2',
-        'package3',
+def get_latest_packages():
+    return [
+        {"name": "flask", "version": "1.2.3"},
+        {"name": "sqlalchemy", "version": "2.2.0"},
+        {"name": "passlib", "version": "3.0.0"}
     ]
-    return flask.render_template("index.html", packages = test_packages) #index.html is the view or the view template
+
+@app.route("/")
+def index():
+    test_packages = get_latest_packages()
+    return flask.render_template("index.html", packages = test_packages)
+
+@app.route("/about")
+def about():
+    return flask.render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
