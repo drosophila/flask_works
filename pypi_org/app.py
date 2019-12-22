5faf8efbdf3d27b71c7b18478062f0f16e852968
drosophila <drosophilaris@gmail.com>
2019-12-22 00:11:00 -0600

    register_blueprint

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -1,20 +1,16 @@
 import flask
-
-from infrastructure.view_modifiers import response
-import services.package_service as package_service
 app = flask.Flask(__name__)
 
-
-@app.route("/")
-@response(template_file="/home/index.html")
-def index():
-    test_packages = package_service.get_latest_packages()
-    return {"packages":test_packages}
-
-@app.route("/about")
-@response(template_file="/home/about.html")
-def about():
-    return {}
-
-if __name__ == "__main__":
+def main():
+    register_blueprints()
     app.run(debug = True) 
+
+
+def register_blueprints():
+    from views import home_views
+    app.register_blueprint(home_views.blueprint)
+    
+
+
+if __name__ == "__main__": main()
+    
