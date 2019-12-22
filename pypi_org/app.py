cbb674efc1d8219549d27645a286568ed1f2c9a5
drosophila <drosophilaris@gmail.com>
2019-12-22 00:26:40 -0600

    project/<package_name>

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -8,8 +8,10 @@ def main():
 
 def register_blueprints():
     from views import home_views
+    from views import package_views
+
     app.register_blueprint(home_views.blueprint)
-    
+    app.register_blueprint(package_views.blueprint)
 
 
 if __name__ == "__main__": main()
