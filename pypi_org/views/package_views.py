4c78d60b05db14a28f22a3bcbcfc6af716cfc99f
drosophila <drosophilaris@gmail.com>
2019-12-30 11:42:03 -0600

    added rank in package_views.py

--- pypi_org/views/package_views.py
+++ pypi_org/views/package_views.py
@@ -6,6 +6,15 @@ from infrastructure.view_modifiers import response
 blueprint = flask.Blueprint("packages", "__name__", template_folder = "templates")
 
 @blueprint.route("/project/<package_name>")
-#@response(template_file="packages/details.html")
+#@response(template_file="details.html")
 def package_details(package_name: str):
     return f"pacakge detail for {package_name}"
+
+@blueprint.route("/<int:rank>")
+def popular(rank: int):
+    if rank==1:
+        return "The details for the #1 most popular package"
+    elif rank==2:
+        return "The details for the 2nd most popular package"
+    else:
+        return f"The details for the {rank}th most popular package"
